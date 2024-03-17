3b265e4c7ce351c7b6f8f56f3e70e5c947450969
chohyunji1007 <145529997+chohyunji1007@users.noreply.github.com>
2024-03-17 16:08:29 +0900

    Task Score: 66% | Correctness Score: 100%

--- MaxCounters/MaxCounters.js
+++ MaxCounters/MaxCounters.js
@@ -3,19 +3,15 @@
 
 function solution(N, A) {
     // Implement your solution here
-    let arr=[];
-    for(let i=0; i<N;i++){
-        arr.push(0);
-    }
+    let arr = new Array(N).fill(0);
     // console.log(arr);
     for(let i=0; i<A.length; i++){
         if(A[i] == N+1){ //모두 최댓값으로
             let max_v = Math.max(...arr);
             arr=[];
             // console.log('max =', max_v);
-            for(let i=0; i<N;i++){
-                arr.push(max_v);
-            }
+            arr = Array(N).fill(max_v);
+            
         }else{
             arr[A[i]-1]+=1;
             // console.log(arr);
