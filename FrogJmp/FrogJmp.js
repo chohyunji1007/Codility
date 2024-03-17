529c8f1f42889c20da1bf0147389c2c42cfe7955
chohyunji1007 <145529997+chohyunji1007@users.noreply.github.com>
2024-03-17 15:46:35 +0900

    Task Score: 100% | Correctness Score: 100%

--- FrogJmp/FrogJmp.js
+++ FrogJmp/FrogJmp.js
@@ -3,9 +3,9 @@
 
 function solution(X, Y, D) {
     // Implement your solution here
-    // x 현재위치, y 이동할 위치(크거나 같으면 됨), d 점프 거리
     if(X>=Y){
         return 0;
+    }else{
+        return Math.ceil((Y-X)/D);
     }
-    return Math.ceil(Y/D);
 }
